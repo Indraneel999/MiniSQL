281779d1c613b24571c24650e0ffb72a2757061b
Indraneeldas <indraneel.das@students.iiit.ac.in>
2019-01-27 06:01:35 +0000

    maps for attribute and table added

--- minisql.cpp
+++ minisql.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+bool is_file_exist(string fileName)		// check if a file exist
+{
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+
 bool icmp(char *a, char *b)				// string insensetive compare
 {
 	if(strlen(a) != strlen(b))
@@ -19,7 +25,7 @@ bool icmp(char *a, char *b)				// string insensetive compare
 	return true;
 }
 
-vector<string> find_num_of_tables(int argc,char *argv[])
+vector<string> find_tables(int argc,char *argv[])
 {
 	vector<string> tab_name;
 	string table,temp;
@@ -80,31 +86,120 @@ vector<string> find_num_of_tables(int argc,char *argv[])
 
 	return tab_name;	
 }
+
+void read_metadata(map <string,int> &table_id,map <string,string> &attr,vector<vector<string> > &table_attr)
+{
+	int flag = 0,xflag, tab_id;
+	fstream md;
+	md.open("metadata.txt");
+
+	string line,tab_name;
+
+	while(md>>line)
+	{
+		if(line == "<begin_table>" && flag == 0)
+		{
+			flag = 1;
+		}
+		else if(line == "<end_table>" && (flag == 1 || flag == 2))
+		{
+			flag = 0;
+		}
+		else if(flag == 1)
+		{
+			tab_name = line;
+
+			cout<<"table = "<<line<<"\n";
+			
+			if (table_id.find(tab_name) == table_id.end()) 
+			{
+        		xflag = 0; 
+			}
+			else
+			{
+				xflag = 1;
+				tab_id = table_id.find(tab_name)->second;
+				cout<<"id = "<<tab_id<<"\n";
+			}
+
+			flag = 2;
+		}
+		else if(xflag == 1)
+		{
+			cout<<line<<"\n";
+			
+			table_attr[tab_id].push_back(line);
+			
+			std::map<string, string>::iterator it = attr.find(line); 
+			if (it != attr.end()) 
+			{
+				 it->second = "0";				 
+			}
+			else
+			{
+				attr.insert({line,tab_name});
+			}
+
+
+			
+
+		}
+		
+	}
+
+
+}
+
+
 int main(int argc, char *argv[])
 {
+	map <string,int> table_id;
+	map <string,string> attr;
+	vector<vector<string> > table_attr(64);
 	char command[20];
 	strcpy(command,argv[1]);
-	cout<<"argc = "<<argc<<"\n";
 	int no_of_tables;
 	vector<string> tab_name;
-
-
+	fstream file[64];
+		
+	string temp; 				// delete
 
 	if (icmp("select", command))
 	{
 	
-		tab_name = find_num_of_tables(argc,argv);
+		tab_name = find_tables(argc,argv);
 
 		for(int i = 0; i<tab_name.size();i++)
 		{
-			cout<<i<<".  "<<tab_name[i]<<"\n";
+			string fname;
+			fname = tab_name[i] + ".csv";		
+			if(!is_file_exist(fname))
+			{
+				cout<<"Table does not exist";
+				return 0;
+			}
+			file[i].open(fname);
+
+			if (table_id.find(tab_name[i]) != table_id.end()) 
+			{
+				cout<<"Ambiguous table name";
+        		return 0; 
+			}
+
+
+			table_id.insert({tab_name[i],i});
+
 		}
 
+		if(tab_name.size() > 64)
+		{
+			cout<<"Too many tables (Max limit = 64)";
+		}
 
-
-
-
-
+		read_metadata(table_id,attr,table_attr);
+		
+		cout<<table_attr[1][2];
+		
 
 
 
